a69ec919866c3419d034b5db0addc9795d80e823
CEEPREL <ceeprel3@gmail.com>
2025-01-23 21:25:50 +0100

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="h-screen bg-gradient-to-b from-[#7D95EE] to-[#F56476]">
+      page
+    </div>
+  );
+}
+
+export default page;
