659832c2e64bf7734adbafa8de90c278279bfe19
CEEPREL <ceeprel3@gmail.com>
2025-01-25 19:56:32 +0100

    update

--- src/app/admin/(dashboards)/revenue/page.tsx
+++ src/app/admin/(dashboards)/revenue/page.tsx
@@ -1,24 +1,59 @@
+"use client";
+import Dropdown from "@/components/Dropdown";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 function Revenue() {
+  const states = [
+    { code: "CA", name: "California" },
+    { code: "TX", name: "Texas" },
+    { code: "NY", name: "New York" },
+    { code: "FL", name: "Florida" },
+  ];
+  const [selectedState, setSelectedState] = useState("");
   return (
     <div className="w-[100%] rounded-3xl p-5 h-[88%] bg-white ">
-      <h1 className=" font-semibold">Revenue</h1>
-
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="flex justify-center items-center w-48 h-48  rounded-full bg-gradient-to-t from-white to-gray-100">
-          <Image
-            className=" top-3 left-1"
-            width={100}
-            height={100}
-            alt=""
-            src={"/icons/revenue.svg"}
-          />
+      {states.length >= 1 && (
+        <div className="">
+          <h1 className=" font-semibold">Revenue</h1>
+          <div className="flex flex-row">
+            <Dropdown
+              showSearch
+              className="bg-white rounded-full w-1/4"
+              label="Select State"
+              options={states}
+              placeholder={states[0].name}
+              onSelect={setSelectedState}
+              getLabel={(state) => state.name}
+              getSubLabel={(state) => state.code}
+            />
+            <Dropdown
+              showSearch
+              className="bg-white rounded-full w-1/4"
+              label="Select State"
+              options={states}
+              placeholder={states[0].name}
+              onSelect={setSelectedState}
+              getLabel={(state) => state.name}
+              getSubLabel={(state) => state.code}
+            />
+          </div>
         </div>
-
-        <h2>No sales recored yet</h2>
-      </div>
+      )}
+      {states.length <= 0 && (
+        <div className="flex flex-col justify-center items-center w-full h-full">
+          <div className="flex justify-center items-center w-48 h-48  rounded-full bg-gradient-to-t from-white to-gray-100">
+            <Image
+              className=" top-3 left-1"
+              width={100}
+              height={100}
+              alt=""
+              src={"/icons/revenue.svg"}
+            />
+          </div>
+          <h2>No sales recored yet</h2>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import React, { useState, forwardRef, useRef, useEffect } from "react";
+import clsx from "clsx";
+import Image from "next/image";
+
+interface DropdownProps<T> {
+  label: string;
+  options: T[];
+  placeholder: string;
+  onSelect: (value: string) => void;
+  className?: string;
+  showSearch?: boolean;
+  getLabel?: (option: T) => string;
+  getSubLabel?: (option: T) => string;
+}
+
+const Dropdown = <T,>({
+  label,
+  options,
+  placeholder,
+  onSelect,
+  className,
+  showSearch,
+  getLabel = (option) => String(option),
+  getSubLabel = (option) => String(option),
+}: DropdownProps<T>) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState("");
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSelect = (value: string) => {
+    setSelected(value);
+    onSelect(value);
+    setIsOpen(false);
+    setSearchTerm("");
+  };
+
+  const filteredOptions = options.filter((option) =>
+    getLabel(option).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+  const handleWindowBlur = () => {
+    setIsOpen(false);
+  };
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
+    window.addEventListener("blur", handleWindowBlur);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
+      window.removeEventListener("blur", handleWindowBlur);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={dropdownRef}
+      className={clsx("relative opacity-100 mt-4", className)}
+    >
+      {/* <label className="block text-gray-700 font-medium">{label}</label> */}
+      <div
+        className="border rounded-full flex justify-between  p-2 cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {selected || placeholder}{" "}
+        <Image
+          src={"/icons/filled_dropdown.svg"}
+          width={20}
+          height={20}
+          alt=""
+        />
+      </div>
+      {isOpen && (
+        <div className="absolute z-50 bg-white border rounded mt-1 w-full shadow">
+          {/* {showSearch === true && (
+            <input
+              className="w-full rounded-full px-2 focus:bg-blue-50 focus:rounded-full"
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              //   placeholder="Search Airport"
+            />
+          )} */}
+          <div className=" h-48 overflow-y-scroll">
+            {filteredOptions.map((option, index) => (
+              <div
+                key={index}
+                className="p-2 bg-white hover:bg-gray-100 cursor-pointer"
+                onClick={() =>
+                  handleSelect(getLabel ? getLabel(option) : (option as string))
+                }
+              >
+                <h1>
+                  {getLabel ? getLabel(option) : (option as string)}{" "}
+                  {getSubLabel && typeof option !== "string" && (
+                    <span className="italic">{getSubLabel(option)}</span>
+                  )}
+                </h1>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className,
+      placeholder,
+      type = "text",
+      value,
+      onClick,
+      onChange,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <input
+        ref={ref}
+        className={`border px-3 py-2 rounded ${className}`}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onClick={onClick}
+        onChange={onChange}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
